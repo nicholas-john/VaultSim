184ae80592006098eafb75bc7c4e9ddfaf5cf6a5
Nick John <nick.john700@gmail.com>
2022-11-16 19:25:38 -0600

    Edited initialization.

--- littlesprings.py
+++ littlesprings.py
@@ -13,22 +13,22 @@ import matplotlib.animation as animation
 class Vault:
     
     def __init__(
+            self,
             L=4, # length of pole in meters
             poledens=.1, # kg / m
             initial_veloc=[17,2], # initial velocity at top of pole
-            initial_theta=np.pi-.4, # anglw of pole from ground
-            npts=100, # number of points in discretization of pole
-            dtime=.0001, # length of time step in simulation
+            initial_theta=np.pi-.4, # angle of pole from ground
+            npts=100, # number of length elements in discretization of pole
+            dtime=.01, # length of time step in simulation
             ntime=6000, # number of time steps in simulation
-            stopping_tol=.001, # how close to unbent for stopping simulation
+            stopping_tol=.001, # how close to unbent to stop simulation
             plot_interval=100, # how many time steps between plot frames
             E=1000, # Elasticity constant
-            I=.01, # radius of pole's cross sesction
+            I=.03, # radius of pole's cross sesction
             name="vault" # file name for the gif
                  ):
         
         self.L = L
-        self.pointmass = pointmass
         self.initial_veloc = np.array(initial_veloc)
         self.npts = npts
         self.dtime = dtime
@@ -44,8 +44,6 @@ class Vault:
         self.s = np.linspace(0, self.L, self.npts)
         self.posx = self.s * np.cos(initial_theta)
         self.posy = self.s * np.sin(initial_theta)
-        
-        self.TOTAL_ENERGY = simple_energy(pointmass, 
-                                          self.initial_veloc, 
-                                          self.posy[-1])
         self.velocity = self.initial_veloc 
+        
+        
